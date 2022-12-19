84a6d6ac5ebc836d49bb9dd1049abfaf489974cc
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-19 11:33:48 -0600

    adds while loop assignment

---
+++ 00 Basic_C#_Programs/WhileAssignment/WhileAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhileAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // while loop, count from 0 to 4
+            int count = 0;
+            while (count <= 5) // loop while count is less than or equal to 5
+            {
+                Console.WriteLine(count);
+                count++; // +1 to count every loop
+            }
+
+            // do-while loop, countdown from 10 to 0
+            int countdown = 10;
+            do
+            {
+                Console.WriteLine(countdown);
+                countdown--; // -1 to countdown every loop
+            }
+            while (countdown >= 0); // loop while countdown is greater than or equal to 0
+            Console.Read();
+        }
+    }
+}
