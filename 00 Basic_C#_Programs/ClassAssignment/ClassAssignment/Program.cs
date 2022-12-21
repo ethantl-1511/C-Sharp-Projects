9489a52e9314a24771366a34cc9181778a433c54
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-21 14:58:39 -0600

    adds ClassAssignment

---
+++ 00 Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ClassAssignment.cAssignment(number); // instantiate class/call method on number
+
+
+            // output parameter call
+            int outputData;
+            ClassAssignment.cAssignment(out outputData);
+            Console.WriteLine("Output data: " + outputData); // print the data
+
+            Console.ReadLine(); // display the output
+        }
+    }
+
+    static class ClassAssignment // create a class, declared static
+    {
+        public static void cAssignment(int someData) // create a void method that outputs an integer
+        {
+            int methodDivide = someData / 2; // Have the method divide the data passed to it by 2.
+            Console.WriteLine("Your number / 2 = " + methodDivide);
+        }
+
+        public static void cAssignment(out int otherData) // create a method with output parameters, overload a method
+        {
+            otherData = 100;
+        }
+    }
+}
--- 00 Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
+++ 00 Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
@@ -8,7 +8,7 @@ namespace TwentyOne
 {
     public class Deck
     {
-        public Deck()
+        public Deck() // constructor
         {
             Cards = new List<Card>();
             List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
@@ -25,6 +25,23 @@ namespace TwentyOne
                 }
             }
         }
-        public List<Card> Cards { get; set; }
+        public List<Card> Cards { get; set; } // property
+
+        public void Shuffle(int times = 1) // shuffle method
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
     }
 }
