b8d3ff76395b701dd5c6019544ceecea56989284
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-21 09:55:51 -0600

    adds MainMethodAssignment

---
+++ 00 Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int AddResult = SimpleMath.Add(num); // instantiate the class and call the one method
+            Console.WriteLine("10 + 100: " + AddResult); // display results
+
+            double dnum = 1.14515627;
+            double DoubleResult = SimpleMath.Add(dnum); // instantiate the class and call the one method
+            Console.WriteLine("1.14515627 + 100.25: " + DoubleResult); // display results
+
+            Console.WriteLine("Give a number to do another math operation on:");
+            uint unum = Convert.ToUInt32(Console.ReadLine());
+            uint uAddResult = SimpleMath.Add(unum);  // instantiate the class and call the one method
+            Console.WriteLine("Your number + 100: " + uAddResult); // display results
+
+            Console.ReadLine();
+        } 
+
+    }
+
+    class SimpleMath // Create a class
+    {
+        public static int Add(int num) // create a method
+        {
+            return (num + 100);
+        }
+
+        public static double Add(double dnum) // add a second method, same name, takes a decimal
+        {
+            return (dnum + 100.25);
+        }
+
+        public static uint Add(uint unum) // create a third method, same name
+        {
+            return (unum + 100);
+        }
+    }
+}
--- 00 Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ 00 Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -12,29 +12,46 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck(); // deck object
-            deck = Shuffle(deck);
+            int timesShuffled = 0;
+            deck = Shuffle(deck, out timesShuffled, 3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shiffled: {0}", timesShuffled);
             Console.ReadLine();
         }
-        // Random function
-        public static Deck Shuffle(Deck deck)
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
 
-            while (deck.Cards.Count > 0)
+        // Random function
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
             }
-            deck.Cards = TempList;
             return deck;
         }
+
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck)
+        //    }
+        //    return deck;
+        //}
     }
 }
