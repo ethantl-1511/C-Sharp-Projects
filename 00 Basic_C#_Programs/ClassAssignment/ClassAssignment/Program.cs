ee6620ca1cdf812c89dea20629965a38e0be3ccc
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-21 19:18:53 -0600

    updates ClassAssignment

--- 00 Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
+++ 00 Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -12,29 +12,17 @@ namespace ClassAssignment
         {
             Console.WriteLine("Please input a number.");
             int number = Convert.ToInt32(Console.ReadLine());
-            ClassAssignment.cAssignment(number); // instantiate class/call method on number
+            FirstClass.cAssignment(number); // instantiate class/call method on number
 
 
             // output parameter call
             int outputData;
-            ClassAssignment.cAssignment(out outputData);
+            FirstClass.cAssignment(out outputData);
             Console.WriteLine("Output data: " + outputData); // print the data
 
+            SecondClass.StaticMethod(""); // static class call.
+
             Console.ReadLine(); // display the output
-        }
-    }
-
-    static class ClassAssignment // create a class, declared static
-    {
-        public static void cAssignment(int someData) // create a void method that outputs an integer
-        {
-            int methodDivide = someData / 2; // Have the method divide the data passed to it by 2.
-            Console.WriteLine("Your number / 2 = " + methodDivide);
-        }
-
-        public static void cAssignment(out int otherData) // create a method with output parameters, overload a method
-        {
-            otherData = 100;
         }
     }
 }
