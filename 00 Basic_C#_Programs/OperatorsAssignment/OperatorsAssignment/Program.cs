164479b056d9f7d6fe8846b76c4100b7f2285d5f
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-22 14:01:11 -0600

    adds OperatorsAssignment

---
+++ 00 Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Individual1 = new Employee(); // instantiate two objects
+            Employee Individual2 = new Employee(); // instantiate two objects
+
+            Individual1.ID = 1;
+            Individual1.FirstName = "John";
+            Individual1.LastName = "Doe";
+            Individual2.ID = 2;
+            Individual2.FirstName = "Jane";
+            Individual2.LastName = "Pro";
+
+            Console.WriteLine("Is Individual 1's ID == to Individual 2's? " + (Individual1.ID == Individual2.ID)); // Should return False
+            Console.WriteLine("Is Individual 1's ID != to Individual 2's? " + (Individual1.ID != Individual2.ID)); // Should return True
+
+            Console.ReadLine();
+        }
+    }
+}
