c9d64bec79fd38f241303b0410409a2c47b29b48
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-22 14:02:09 -0600

    Update Program.cs

--- 00 Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
+++ 00 Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -14,13 +14,16 @@ namespace OperatorsAssignment
             Employee Individual1 = new Employee(); // instantiate two objects
             Employee Individual2 = new Employee(); // instantiate two objects
 
+            // assign values to their properties
             Individual1.ID = 1;
             Individual1.FirstName = "John";
             Individual1.LastName = "Doe";
+
             Individual2.ID = 2;
             Individual2.FirstName = "Jane";
             Individual2.LastName = "Pro";
 
+            // compare the objects using the overloaded operators, display results
             Console.WriteLine("Is Individual 1's ID == to Individual 2's? " + (Individual1.ID == Individual2.ID)); // Should return False
             Console.WriteLine("Is Individual 1's ID != to Individual 2's? " + (Individual1.ID != Individual2.ID)); // Should return True
 
