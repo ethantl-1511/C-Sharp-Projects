36d3e91528c60ef5f89d12cc8ff4f1f3453f64fc
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-12-26 14:30:01 -0600

    adds Blackjack Game

---
+++ 01 Blackjack Game/TwentyOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        // Main function
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casion. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank); // create player object
+                Game game = new TwentyOneGame(); // create the game
+                game += player; // adds player to game
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Good bye.");
+            Console.ReadLine();
+        }
+    }
+}
